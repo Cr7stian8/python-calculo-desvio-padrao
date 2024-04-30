74b00d1e675834f5cff7529a7f846ca8eb3c84ab
Cristiano Corrêa <90468883+Cr7stian8@users.noreply.github.com>
2024-04-30 10:48:05 -0300

    adicionando um menuzinho bacana

--- script.py
+++ script.py
@@ -1,56 +1,57 @@
 import os
-import sys
 
 def calculo_media(numeros):
-    soma = sum(numeros)
-    u = soma/len(numeros)
-    return u
+    return sum(numeros) / len(numeros)
 
+def calculo_desvios(numeros, media):
+    return [(numero - media) ** 2 for numero in numeros]
 
-def calculo_desvios(numeros, u):
-    lista_desvios = []
-    for numero in numeros:
-        lista_desvios.append((numero-u)**2)
-    return lista_desvios
+def variancia_desvio(desvios):
+    return round(sum(desvios) ** (1/2), 5)
 
+def input_dado():
+    while True:
+        try:
+            return float(input("Digite um número: "))
+        except ValueError:
+            print("\n--> Apenas números são permitidos <--\n")
 
-def variancia_desvio(us):
-    return round(us**(1/2), 3)
+def limpar_tela():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-
-def input_dado(numeros):
-    try:
-        numeros.append(float(input("numero: ")))
-        os.system('clear')
-        return numeros
-    except ValueError:
-        os.system('clear')
-        print("\n\n\n--> somante numeros <--\n\n\n")
-
-    
+def mostrar_menu():
+    print("1. Adicionar número")
+    print("2. Calcular média e desvio padrão")
+    print("3. Reiniciar e começar do zero")
+    print("4. Sair")
+    return input("Escolha uma opção: ")
 
 def main():
-    os.system('clear')
     numeros = []
-    lista_desvios = []
-    print("numeros",numeros)
-    #return
+
     while True:
-        # os.system('clear')
-        numeros = input_dado(numeros)
+        limpar_tela()
+        opcao = mostrar_menu()
 
-        u = calculo_media(numeros)
-        print("Numeros", numeros)
-        print("Média: ", u)
-        lista_desvios = calculo_desvios(numeros, u)
+        if opcao == '1':
+            numeros.append(input_dado())
+        elif opcao == '2':
+            if numeros:
+                media = calculo_media(numeros)
+                desvios = calculo_desvios(numeros, media)
+                print("\nNúmeros:", numeros)
+                print("Média: {:.5f}".format(media))
+                print("Desvio padrão: {:.5f}".format(variancia_desvio(desvios)))
+            else:
+                print("\nNenhuma número foi adicionado.")
+        elif opcao == '3':
+            numeros.clear()
+        elif opcao == '4':
+            break
+        else:
+            print("\nOpção inválida!")
 
-        print("Desvio pradrão-> ", lista_desvios)
-        print('{}'.format(variancia_desvio(calculo_media(lista_desvios))))
+        input("\nPressione Enter para continuar...")
 
-
-# try:
-#     numeros = sys.argv[1]
-# except IndexError:
-#     numeros = False
-
-main()
+if __name__ == "__main__":
+    main()
